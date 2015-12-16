c005352bc678742eac5dd60afa61bf3fdded2ac4
qiqipchy <980260193@qq.com>
2015-12-16 12:07:32 +0800

    Create 164_Maximum_Gap.java

---
+++ 164_Maximum_Gap.java
@@ -0,0 +1,13 @@
+public class Solution {
+   public int maximumGap(int[] nums) {
+		if (nums.length < 2)
+			return 0;
+		Arrays.sort(nums);
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i + 1] - nums[i] > max)
+				max = nums[i + 1] - nums[i];
+		}
+		return max;
+	}
+}
